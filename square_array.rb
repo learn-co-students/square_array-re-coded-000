3754240d310ea5ae9b5af4006e60300d53ecaf49
MOHAMMAD DAYLAKI <mohammad.daylaky@gmail.com>
2016-12-02 22:06:11 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,14 @@
 def square_array(array)
   # your code here
+  def square_array(array)
+    array.each do |number|
+      square = number*number
+      num_dex =array.index(number)
+      array.delete(number)
+      array.insert(num_dex ,square)
+
+    end
+    array
+  end
+
 end
